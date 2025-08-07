affa005359a5f0f0877c4562525f7d0f12eeb719
piyushrasne <piyushrasne@gmail.com>
2025-08-07 14:55:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -142,5 +142,7 @@ def logout():
     session.pop('admin', None)
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
